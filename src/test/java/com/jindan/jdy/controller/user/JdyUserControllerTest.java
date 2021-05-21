a030fef3fa9380aa2921ebab671389f7f5980233
JINDAN-LAB <3258310932@qq.com>
2021-05-21 10:27:42 +0800

    添加单元测试类

---
+++ src/test/java/com/jindan/jdy/controller/user/JdyUserControllerTest.java
@@ -0,0 +1,56 @@
+package com.jindan.jdy.controller.user;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * Created by 13348 on 2021/5/21.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JdyUserControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mvc;
+    private MockHttpSession session;
+
+    @Before
+    public void setupMockMvc(){
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build(); //初始化MockMvc对象
+        session = new MockHttpSession();
+//        User user =new User("root","root");
+//        session.setAttribute("user",user); //拦截器那边会判断用户是否登录，所以这里注入一个用户
+    }
+
+    /**
+     * 获取教程测试用例
+     * @throws Exception
+     */
+    @Test
+    public void qryLearn() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/jdyUser/info/023293cd7934a5ef394ca415078aabfa")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .session(session)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.author").value("嘟嘟MD独立博客"))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Spring Boot干货系列"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+}
